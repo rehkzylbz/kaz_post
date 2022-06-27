ebe1e515baac43e9cbf0cbf6478899b12c252f6d
rehkzylbz <rehkzybz@yandex.com>
2022-06-27 19:46:01 +0300

    main view edited

--- app/views/v_index.php
+++ app/views/v_index.php
@@ -6,7 +6,7 @@
         <title>TK</title>
         <link rel="shortcut icon" href="app/resources/images/favicon.ico"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    </head>>
+    </head>
     <body>
         <div class="container pt-3">
             <h1>Запрос по трек номеру</h1>
@@ -17,13 +17,13 @@
                     <label for="track_number" class="col-sm-2 col-form-label">Введите номер: </label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="track_number" name="track_number">
-                    </div>>
+                    </div>
                 </div>                   
-            </form>>
+            </form>
 
             <button id="send_form" class="btn btn-primary">Запросить</button>
-        </div>>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <script src="app/resources/js/front.js"></script>
     </body>
-</html>>
+</html>
